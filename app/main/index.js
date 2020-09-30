29118edef2da6e109906d1c43a025d0fe6b38c29
tank <848464730@qq.com>
2020-09-30 16:56:39 +0800

    重写

---
+++ app/main/index.js
@@ -0,0 +1,78 @@
+const { app, dialog, BrowserWindow, ipcMain, Menu } = require('electron');
+const path = require('path');
+let win = null;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 600,
+        height: 200,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    Menu.setApplicationMenu(null);
+    win.webContents.openDevTools(true);
+
+    win.loadFile(path.resolve(__dirname, '../renderer/stream.html'));
+
+}
+
+// peer-open
+ipcMain.on('on-peer-open', (e, id) => {
+    onPeerOpen(id);
+});
+ipcMain.on('on-peer-connection', onPeerConnection);
+ipcMain.on('on-peer-connect', (e, id) => {
+    onPeerConnect(id)
+});
+ipcMain.on('on-peer-connected', (e, id) => {
+    onPeerConnected(id)
+});
+ipcMain.on('on-peer-stream', (e, id) => {
+    onPeerStream(id)
+});
+
+
+function onPeerOpen(id) {
+    console.log('peer 创建成功', id)
+}
+
+function onPeerConnection() {
+    console.log('接受连接成功')
+}
+
+function onPeerConnect(id) {
+    console.log('开始连接对象=>', id);
+}
+
+function onPeerConnected(id) {
+    console.log('连接对象成功=>', id);
+}
+
+function onPeerStream() {
+    win.setSize(1280, 1024, true);
+}
+
+app.on('window-all-closed', (e) => {
+    e.preventDefault();
+    dialog.showMessageBox({
+        type: "question",
+        buttons: [
+            '确认',
+            '取消'
+        ],
+        title: '是否退出？',
+        message: '退出后远程控制将关闭',
+    })
+    .then(res => {
+        console.log(res);
+    })
+    if (process.platform !== 'darwin') {
+        //app.quit();
+    }
+    return true;
+});
+
+app.whenReady()
+    .then(createWindow);
---
+++ app/renderer/render.js
@@ -0,0 +1,193 @@
+const $ = (selector) => {
+  return document.querySelector(selector);
+}
+const video = $('#video');
+const $localCode = $('#localCode');
+const $remoteCode = $('#remoteCode');
+const $connect = $('#connect');
+const { desktopCapturer, ipcRenderer } = window.require('electron');
+
+// 获取桌面流
+async function getScreenStream() {
+  const sources = await desktopCapturer.getSources({
+    types: ["screen"],
+  });
+  return new Promise((resolve, reject) => {
+  navigator.mediaDevices
+      .getUserMedia({
+          audio: false,
+          video: {
+              mandatory: {
+              chromeMediaSource: "desktop",
+              chromeMediaSourceId: sources[0].id,
+              maxWidth: window.screen.width,
+              maxHeight: window.screen.height,
+              },
+          },
+      })
+      .then((stream) => {
+          resolve(stream);
+      })
+      .catch((err) => {
+          alert('获取屏幕流失败');
+          console.error(err);
+          reject(err);
+      });
+  });
+}
+
+function randomCode() {
+  if (localStorage['localCode']) {
+      return localStorage['localCode'];
+  } else {
+      let localCode = Math.floor(Math.random() * (999999 - 100000)) + 1000000;
+      localStorage['localCode'] = localCode;
+      return localCode;
+  }
+}
+
+const peer = new Peer(randomCode(), {
+  host: '192.168.96.128',
+  port: 9000,
+  path: '/',
+  debug: 4
+});
+let localConn = null;
+let remoteConn = null;
+let remote = '';
+
+// 重置界面
+function resetView() {
+  $('.main').style.display = 'block';
+  $('#beControl').style.display = 'none';
+}
+
+// 初始化成功
+peer.on('open', id => {
+  console.log('peer ID: ', id);
+  $localCode.innerText = id;
+  ipcRenderer.send('on-peer-open', id);
+});
+
+// 发生错误
+peer.on('error', err => {
+  alert('错误:', err);
+  resetView();
+});
+// 连接中断peer.reconnect();
+peer.on('disconnected', err => {
+  alert('连接已终止');
+  resetView();
+});
+// 连接关闭 peer.destroyed();
+peer.on('close', err => {
+  console.log('peer close');
+  alert('连接已终止');
+  resetView();
+});
+
+// 接受连接
+peer.on('connection', conn => {
+  ipcRenderer.send('on-peer-connection');
+  remoteConn = conn;
+  console.log('已接受连接', remoteConn);
+  remoteConn.on('data', data => {
+      console.log('收到消息: ', data);
+      if (data.remoteCode) {
+          $('#beControl').style.display = 'block';
+          $('.main').style.display = 'none';
+          $('#beControlCode').innerText = data.remoteCode;
+      }
+  });
+});
+
+// 处理远程相应stream
+function handleRemoteStream(stream) {
+  // 隐藏main界面
+  $('.container').style.display = 'none';
+  ipcRenderer.send('on-peer-stream');
+  video.srcObject = stream;
+  video.onloadedmetadata = (e) => {
+      video.play();
+  }
+}
+
+// 接受流数据
+peer.on('call', function(call) {
+  getScreenStream()
+      .then(function(stream) {
+          call.answer(stream); // Answer the call with an A/V stream.
+          call.on('close', () => {
+            alert('r stream close');
+          });
+          call.on('error', () => {
+            alert('r stream error');
+          });
+      })
+      .catch(function(err) {
+          console.log('Failed to get local stream' ,err);
+      });
+});
+
+// 发送流数据
+function onCallStream(id) {
+  getScreenStream()
+      .then(function(stream) {
+          let call = peer.call(id, stream);
+          call.on('stream', function(remoteStream) {
+              handleRemoteStream(remoteStream);
+          });
+          call.on('close', () => {
+            alert('c stream close');
+          });
+          call.on('error', () => {
+            alert('c stream error');
+          });
+      })
+      .catch(function(err) {
+          console.log('Failed to get local stream' ,err);
+      });
+}
+
+// 回复消息
+function reSendData(data)  {
+  if (remoteConn) {
+      remoteConn.send(data);
+  }
+}
+
+// 发送消息
+function sendData(data)  {
+  if (localConn) {
+      // Send messages
+      localConn.send(data);
+  }
+}
+
+// 建立连接
+function peerConnect(id) {
+  localConn = peer.connect(id);
+  ipcRenderer.send('on-peer-connect', id);
+  // 建立连接-成功
+  localConn.on('open', () => {
+      console.log('建立连接-成功');
+      ipcRenderer.send('on-peer-connected', id);
+      localConn.send({
+          remoteCode: id
+      });
+      localConn.on('data', function(data) {
+          console.log('接受数据', data);
+      });
+  });
+}
+
+// 开始连接点击
+$connect.addEventListener('click', e => {
+  const remoteCode = $remoteCode.value.trim();
+  if (remoteCode === '') {
+      alert('请输入远程设备码');
+      return;
+  }
+  peerConnect(remoteCode);
+  onCallStream(remoteCode);
+})
